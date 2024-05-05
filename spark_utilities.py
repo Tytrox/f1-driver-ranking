003a7350114541dfafdd8e6c6876fe57e2ff715f
jw309 <jw309@st-andrews.ac.uk>
2024-05-05 14:42:02 +0100

    feat: add pyspark utility script

---
+++ spark_utilities.py
@@ -0,0 +1,22 @@
+import os.path
+from functools import lru_cache
+from pyspark.sql import SparkSession, DataFrame
+from os import path
+from convert_csv_to_parquet import data_directory
+
+spark = None
+
+
+@lru_cache
+def get_df_from_file(filename: str) -> DataFrame:
+    global spark
+    filepath = path.join(data_directory(), f"{filename}.parquet")
+
+    if not os.path.exists(filepath):
+        raise ValueError(
+            "The provided filename does not correspond to a valid path")
+
+    if spark is None:
+        spark = SparkSession.builder.getOrCreate()
+
+    return spark.read.parquet(filepath)
